d234d5aede575a1600df0c55dd1c2a9c8477068d
fufubear <fufu258w@gmail.com>
2018-12-14 21:04:43 +0800

    SiteFooter update

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer position-absolute">
+    <p>Copyright © 2018 FUFUBEAR All rights reserved</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SiteFooter'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .footer {
+    bottom: 0;
+    width: 100%;
+  }
+  .footer {
+    text-align: center;
+  }
+</style>

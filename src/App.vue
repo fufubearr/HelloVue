c8dd5460cba31377ff98c1c56bc93f8b4d76db12
fufubear <fufu258w@gmail.com>
2018-12-17 15:09:27 +0800

    ＋ElementUI

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
-     <!-- vue logo -->
+    <!-- vue logo -->
     <img src="./assets/logo.png" id="logo">
     <!-- 導覽列 -->
     <nav>
       <router-link to="/">首頁</router-link>
       <router-link to="/down">連結二</router-link>
+      <router-link :to="{name:'HelloWorld'}">首頁</router-link>
+      <router-link :to="{name:'CatDown'}">貓躺著</router-link>
     </nav>
+    <el-row>
+      <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+    </el-row>
     <!-- 路由渲染區域 -->
     <!-- 路由匹配到的组件將會渲染在這 -->
     <router-view/>
@@ -36,5 +64,31 @@ export default {
 }
 #logo{
   max-width: 60px;
+}
+.el-row {
+  margin-bottom: 20px;
+  /* &:last-child {
+    margin-bottom: 0;
+  } */
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App' // 載入 App 元件
-import router from './router' // 載入路由
+import ElementUI from 'element-ui' // 載入 Element
+import 'element-ui/lib/theme-chalk/index.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import App from './App' // 載入 App 元件
+import router from './router' // 載入路由
 
 Vue.config.productionTip = false // 開發環境提示，預設為 false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(App) // components + template = render
 })
